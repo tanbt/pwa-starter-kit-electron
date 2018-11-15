d803fee3604e4465a6422259584c61d75b21970b
Tan Bui <tan@vaadin.com>
2018-11-15 16:34:41 +0200

    fix polymer config
    donot use absolute path in basePath and service-worker scope

--- src/components/my-view404.ts
+++ src/components/my-view404.ts
@@ -10,6 +10,14 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
 
 import { html } from '@polymer/lit-element';
 import { PageViewElement } from './page-view-element.js';
+import '@vaadin/vaadin-split-layout/vaadin-split-layout.js';
+import '@vaadin/vaadin-designer-viewport/vaadin-designer-viewport.js';
+import '@vaadin/vaadin-designer-paper/vaadin-designer-paper.js';
+import '@vaadin/vaadin-designer-palette/vaadin-designer-palette.js';
+//import '@vaadin/vaadin-designer-license-dialog/vaadin-designer-license-dialog.js';
+import '@vaadin/vaadin-dnd/vaadin-dnd.js';
+import '@vaadin/vaadin-designer-outline/vaadin-designer-outline.js';
+import '@vaadin/vaadin-designer-properties/vaadin-designer-properties.js';
 
 // These are the shared styles needed by this element.
 import { SharedStyles } from './shared-styles.js';
@@ -18,10 +26,31 @@ class MyView404 extends PageViewElement {
   protected render() {
     return html`
       ${SharedStyles}
+      <vaadin-split-layout id="split-layout-container">
+        <vaadin-designer-viewport id="viewport" content-width="{{paperWidth}}" content-height="{{paperHeight}}"
+          editable-element="[[editableElementTag]]">
+          <vaadin-designer-paper id="paper" selected="{{selected}}" mode="[[mode]]" element-resolver="[[resolver]]" iframe>
+            <vaadin-dnd id="vaadinDnd" editable-element="[[editableElementTag]]" broadcaster="[[dndBroadcaster]]"
+              internal-id-name="[[designerSourceId]]" enable-dnd="[[isEditMode]]">
+              <iframe id="design-element-wrapper"></iframe>
+            </vaadin-dnd>
+          </vaadin-designer-paper>
+        </vaadin-designer-viewport>
+      
+        <vaadin-split-layout id="components-wrapper">
+          <vaadin-designer-palette id="palette" web-component-imports="[[webComponentImports]]"></vaadin-designer-palette>
+      
+          <vaadin-split-layout orientation="vertical">
+            <vaadin-designer-outline id="outline" items="[[outlineItems]]" selected-id="[[outlineSelectedId]]"
+              selection-changed-callback="[[_boundOutlineSelectionChangedCallback]]"></vaadin-designer-outline>
+            <vaadin-designer-properties id="properties" items="[[propertiesItems]]"></vaadin-designer-properties>
+          </vaadin-split-layout>
+        </vaadin-split-layout>
+      </vaadin-split-layout>
       <section>
         <h2>Oops! You hit a 404</h2>
         <p>The page you're looking for doesn't seem to exist. Head back
-           <a href="/">home</a> and try again?
+          <a href="/">home</a> and try again?
         </p>
       </section>
     `
